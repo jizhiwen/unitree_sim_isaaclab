a8562fffbf7efd3f87c193e983d1e7c3f60c623a
jizhiwen <ji.zhiwen@outlook.com>
2025-09-08 13:38:46 +0800

    upload base_termination_paint_cylinder module

---
+++ tasks/common_termination/base_termination_paint_cylinder.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2025, Unitree Robotics Co., Ltd. All Rights Reserved.
+# License: Apache License, Version 2.0      
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING
+
+from isaaclab.assets import RigidObject
+from isaaclab.managers import SceneEntityCfg
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedRLEnv
+
+
+def reset_object_estimate(
+    env: ManagerBasedRLEnv,
+    object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
+    min_x: float = -0.42,                # minimum x position threshold
+    max_x: float = 1.0,                # maximum x position threshold
+    min_y: float = 0.2,                # minimum y position threshold
+    max_y: float = 0.7,                # maximum y position threshold
+    min_height: float = 0.5,
+) -> torch.Tensor:
+    
+    return False
